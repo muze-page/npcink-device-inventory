7515678c20bcdd9309096a7043d139ea0d3b9070
Muze-mac <13581261474@163.com>
2023-09-18 13:58:34 +0800

    可用

--- vite/src/components/page/tab/disk.tsx
+++ vite/src/components/page/tab/disk.tsx
@@ -7,44 +7,11 @@ import TabList from "@/components/block/tabList";
 //表头
 const thData = ["型号", "数量（块）"];
 
-//数据
-const tableData = [
-  {
-    type: "128G",
-    sum: 0,
-  },
-  {
-    type: "256G",
-    sum: 0,
-  },
-  {
-    type: "512G",
-    sum: 0,
-  },
-  {
-    type: "1T",
-    sum: 0,
-  },
-  {
-    type: "2T",
-    sum: 0,
-  },
-];
 interface Props {
   data: any;
 }
 const App: React.FC<Props> = ({ data }) => {
-  console.log(data);
-  const objects = [
-    { size: 1024 },
-    { size: 524288 },
-    { size: 134217728 },
-    { size: 268435456 },
-    { size: 536870912 },
-    { size: 968435400 },
-  ];
-
-  const thresholds = {
+  const thresholds: Record<string, number> = {
     "128G": 128,
     "256G": 256,
     "512G": 512,
@@ -52,10 +19,10 @@ const App: React.FC<Props> = ({ data }) => {
     "2T": 2048,
   };
 
-  const arr = [];
+  const arr: { type: string; sum: number }[] = [];
 
-  objects.forEach((obj) => {
-    const sizeInGB = obj.size / (1024 * 1024); // 将 size 从字节转换为 GB
+  data.forEach((obj: { size: number }) => {
+    const sizeInGB = obj.size / (1024 * 1024 * 1024); // 将 size 从字节转换为 GB
 
     for (const type in thresholds) {
       if (sizeInGB < thresholds[type]) {
@@ -70,11 +37,9 @@ const App: React.FC<Props> = ({ data }) => {
     }
   });
 
-  console.log(arr);
-
   return (
     <>
-      <TabList thData={thData} tableData={tableData} />
+      <TabList thData={thData} tableData={arr} />
     </>
   );
 };
