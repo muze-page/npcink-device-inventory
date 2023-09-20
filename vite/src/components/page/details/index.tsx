d9026ee6936ee904fb046a95959a5c874414c891
Muze-mac <13581261474@163.com>
2023-09-20 18:23:18 +0800

    拿到需要的值

--- vite/src/components/page/details/index.tsx
+++ vite/src/components/page/details/index.tsx
@@ -10,11 +10,63 @@ import Property from "@/components/page/details/property";
 const App: React.FC = () => {
   //拿到数据
   const data = useContext(DataContext);
-  console.log(data);
-  const newArray = data.map((item:any) => {
-    return { os: item.os.distro };
-  });
-  console.log(newArray);
+
+  //收集指定数据
+  const calculateTotalSize = (dataArrays: any) => {
+    const totalSize = dataArrays.reduce(
+      (sum: number, obj: { size: number }) => {
+        return sum + obj.size;
+      },
+      0
+    );
+
+    const sizeInGB = totalSize / (1024 * 1024 * 1024); // 将字节转换为GB
+
+    return sizeInGB;
+  };
+
+  //处理数据
+  const updateOSType = (dataArrays: any) => {
+    const updatedData = dataArrays.map(
+      (obj: { dataNew: any; ostype?: any }) => {
+        const parsedData = obj.dataNew;
+        const ostype = parsedData.os.distro; //系统版本
+        const model = parsedData.system.model; //型号
+
+        const cpu = parsedData.cpu.manufacturer; //CPU
+        const memoryData = parsedData.memLayout; //内存数组
+        const memory = calculateTotalSize(memoryData);
+
+        const diskData = parsedData.diskLayout; //内存数组
+        const disk = calculateTotalSize(diskData);
+
+        const meat = {
+          ostype: ostype,
+          cpu: cpu,
+          model: model,
+          memory: Math.floor(memory),//GB 取整
+          disk: Math.floor(disk),//GB 取整
+        };
+        //内存总容量
+        //获取内存数组
+
+        //硬盘总容量
+        return { ...obj, meat };
+      }
+    );
+    //移除多余数组
+    //const updatedData = updatedData.map((obj) => {
+    //  const { dataNew,dataOld, ...rest } = obj;
+    //  return rest;
+    //});
+
+    return updatedData;
+  };
+
+  //处理后的数据
+  const updatedDataArray = updateOSType(data);
+
+  console.log(updatedDataArray);
   //整理，需要，设备类型，Apple 还是Windows，
   /**
    * 昵称
