efc1687215d9bafb087f64f363abb4e149c67c5d
Muze-mac <13581261474@163.com>
2023-09-22 18:10:39 +0800

    可用

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import Index from "@/components/page/index";
+import { ConfigProvider } from "antd";
+
+import zhCN from 'antd/locale/zh_CN';
 
 function App() {
   return (
     <>
+    <ConfigProvider locale={zhCN}>
       <Index />
+      </ConfigProvider>
     </>
   );
 }
--- vite/src/components/page/details/block/change.tsx
+++ vite/src/components/page/details/block/change.tsx
@@ -1,9 +1,9 @@
 /**
  * 设备详情 - 变更记录
  */
-import { Table } from "antd";
+import { Table, Empty } from "antd";
 import type { ColumnsType } from "antd/es/table";
-import {findDifferentKeys} from "@/store/tool"
+import { findDifferentKeys, replaceType } from "@/store/tool";
 
 interface DataType {
   key: string;
@@ -60,6 +60,13 @@ const dataTable: DataType[] = [
   },
 ];
 
+//替换列表
+const replacements = {
+  "os.distro": "系统版本",
+  "diskLayout.1.size": "主硬盘大小",
+  // 其他需要替换的字符串
+};
+
 interface Props {
   data: any;
 }
@@ -72,8 +79,18 @@ const App: React.FC<Props> = ({ data }) => {
   //进行处理
   const differences = findDifferentKeys(dataOld, dataNew);
 
-  
+  //替换关键词
+  function replaceType(difference, replacements) {
+    difference.change = replacements[difference.change];
+  }
+  differences.forEach(difference => {
+    replaceType(difference, replacements);
+  });
 
+
+  
+  console.log(differences);
+  //添加若干参数
   addUniqueIdAndTime(differences);
   console.log(differences);
 
@@ -86,25 +103,26 @@ const App: React.FC<Props> = ({ data }) => {
       obj.time = generateRandomDate();
     });
   }
-  
+
   function generateUniqueId() {
     return Math.random().toString(36).substr(2, 9);
   }
-  
+
   function generateRandomDate() {
     const startDate = new Date(2000, 0, 1); // 开始日期为2000年1月1日
     const endDate = new Date(); // 结束日期为当前日期
-  
-    const randomTimestamp = Math.floor(Math.random() * (endDate - startDate) + startDate.getTime());
+
+    const randomTimestamp = Math.floor(
+      Math.random() * (endDate - startDate) + startDate.getTime()
+    );
     const randomDate = new Date(randomTimestamp);
-  
+
     const year = randomDate.getFullYear();
-    const month = String(randomDate.getMonth() + 1).padStart(2, '0');
-    const day = String(randomDate.getDate()).padStart(2, '0');
-  
+    const month = String(randomDate.getMonth() + 1).padStart(2, "0");
+    const day = String(randomDate.getDate()).padStart(2, "0");
+
     return `${year}-${month}-${day}`;
   }
-
 
   return (
     <>
@@ -112,7 +130,11 @@ const App: React.FC<Props> = ({ data }) => {
         {/**列表 */}
         <div className="mt-1">
           <p className="mb-4 text-base font-bold text-[#333]">硬件信息变更</p>
-          <Table size="small" columns={columns} dataSource={differences} />
+          {differences.length === 0 ? (
+            <Empty />
+          ) : (
+            <Table size="small" columns={columns} dataSource={differences} />
+          )}
         </div>
         {/**下载按钮 */}
       </div>
