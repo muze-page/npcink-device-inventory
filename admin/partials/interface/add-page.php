5b0d51c2aa3685691ca0b639b72cb2d59f822f8e
Muze-mac <13581261474@163.com>
2024-04-20 10:46:00 +0800

    添加页面路由纯数字检测

--- admin/partials/interface/add-page.php
+++ admin/partials/interface/add-page.php
@@ -22,20 +22,27 @@ if (!class_exists('DEMA_Admin_Interface_Add_Page')) {
         {
             //接收传来的值并清理路由参数
             $route = isset($_POST['route']) ? sanitize_text_field($_POST['route']) : null;
-            // 检查路由参数是否为空
+            // 检查路由参数是否为空，是否是纯数字
             if (empty($route)) {
                 return wp_send_json_error([
                     'error' => '需要路由参数',
                 ], 403);
             }
+            // 检查路由参数是否为纯数字
+            if (ctype_digit($route)) {
+                return wp_send_json_error([
+                    'error' => '查询页面地址不能是纯数字',
+                ], 403);
+            }
 
             //检查，是否存在当前路由
-            $state = get_page_by_path($route); //获取此路由信息
+            $state = get_page_by_path($route, OBJECT, ['page']); //获取此路由信息
+
             if ($state) {
                 //返回相关信息
                 return wp_send_json_error([
                     'error' => '页面已存在',
-                    'site' => $state->guid,//页面地址
+                    'site' => $state->guid, //页面地址
                 ], 404);
                 // 页面已经存在，不执行后续操作
             }
