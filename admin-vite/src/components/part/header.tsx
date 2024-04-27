2b7c0d8a170b70fde09712a14dd43a41b58c36cd
Muze-mac <13581261474@163.com>
2024-04-27 18:19:59 +0800

    优化前端搜索页外观

--- admin-vite/src/components/part/header.tsx
+++ admin-vite/src/components/part/header.tsx
@@ -5,7 +5,7 @@ interface Props {
 const App: React.FC<Props> = ({ title }) => {
   return (
     <>
-      <div className="flex justify-between items-center mb-6">
+      <div className="flex justify-between items-center my-4">
         <div className="text-base font-black flex items-center text-zinc-900">
           {title}
         </div>
