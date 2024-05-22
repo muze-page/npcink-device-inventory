75edd70c386cdd30452b07ff2089a9eba3b37ecf
Muze-mac <13581261474@163.com>
2024-05-22 13:57:57 +0800

    添加变更数据下载功能

--- admin-vite/src/components/changeList/index.tsx
+++ admin-vite/src/components/changeList/index.tsx
@@ -1,7 +1,61 @@
 import React, { useState, useEffect } from "react";
 import { searchChangeAllData } from "@/axios";
-import { Table, message } from "antd";
+import { Button, Table, message } from "antd";
 import { DeviceChangeList } from "@/store/interface";
+
+//
+//导出表格
+const exportTable = (jsonData: {}[]) => {
+  // 如果没有拿到值，就此结束
+  if (!jsonData) {
+    return;
+  }
+
+  // 创建一个表格元素
+  const table = document.createElement("table");
+
+  // 添加表头
+  const thead = document.createElement("thead");
+  const headers = Object.keys(jsonData[0]);
+  const headerRow = document.createElement("tr");
+  headers.forEach((headerText) => {
+    const th = document.createElement("th");
+    th.appendChild(document.createTextNode(headerText));
+    headerRow.appendChild(th);
+  });
+  thead.appendChild(headerRow);
+  table.appendChild(thead);
+
+  // 添加数据行
+  const tbody = document.createElement("tbody");
+  jsonData.forEach((rowData: { [x: string]: string }) => {
+    const row = document.createElement("tr");
+    headers.forEach((header) => {
+      const cell = document.createElement("td");
+      cell.appendChild(document.createTextNode(rowData[header]));
+      row.appendChild(cell);
+    });
+    tbody.appendChild(row);
+  });
+  table.appendChild(tbody);
+
+  // 将表格转换为 Excel 文件
+  const blob = new Blob([table.outerHTML], {
+    type: "application/vnd.ms-excel",
+  });
+  const url = URL.createObjectURL(blob);
+
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = "硬件变更数据导出文件.xlsx";
+  link.click();
+
+  // 等待一段时间后释放 URL 对象
+  setTimeout(() => {
+    URL.revokeObjectURL(url);
+  }, 1000);
+};
+
 const App: React.FC = () => {
   const [dataAxios, setDataAxios] = useState([]); //待渲染的值
   // 获取数据并处理
@@ -57,10 +111,17 @@ const App: React.FC = () => {
     },
   ];
 
+  //导出表格数据
+  const exportForm =async () => {
+    await getData();
+    console.log(dataAxios);
+    exportTable(dataAxios);
+  };
   return (
     <>
-      {/* <Button onClick={getData}>获取数据</Button>*/}
       <Table dataSource={dataAxios} columns={columns} />
+      <br />
+      <Button onClick={exportForm}>下载表格</Button>
     </>
   );
 };
