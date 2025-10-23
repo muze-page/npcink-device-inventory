74bb38c21b622b17ef9b68137099df1a2b79c932
Muze-mac <13581261474@163.com>
2025-10-23 19:12:52 +0800

    优化细节

--- vite-admin/src/pages/styleList/drawer/info.tsx
+++ vite-admin/src/pages/styleList/drawer/info.tsx
@@ -2,7 +2,7 @@
  * 自定义设备信息展示
  */
 import { useContext } from "react";
-import { Card, Skeleton } from "antd";
+import { Card, Skeleton, Space } from "antd";
 import { StyleDevice } from "@/type/index";
 
 //跨组件提供方法
@@ -42,70 +42,88 @@ const App: React.FC<Props> = ({ data }) => {
           </a>
         }
         style={{ width: "100%" }}
+        className="bg-slate-200"
       >
-        设备信息：
-        <p>
-          <b>用途：</b>
-          {data.purpose}
-        </p>
-        <p>
-          <b>采购数量：</b>
-          {formatNumber(deviceData.numbers)}
-        </p>
-        <p>
-          <b>采购总价：</b>
-          {formatNumber(deviceData.total)} 元
-        </p>
-        <p>
-          <b>当前状态：</b>
-          {statusLabel(data.state)}
-        </p>
-        <p>
-          <b>分类：</b>
-          {data.category}
-        </p>
-        采购信息：
-        <p>
-          <b>采购人员：</b>
-          {isName ? (
-            deviceData.purchaser
-          ) : (
-            <Skeleton.Input active={true} size={"small"} />
-          )}
-        </p>
-        <p>
-          <b>设备编号：</b>
-          {isName ? (
-            data.number
-          ) : (
-            <Skeleton.Input active={true} size={"small"} />
-          )}
-        </p>
-        <p>
-          <b>使用人员：</b>
-          {isName ? data.name : <Skeleton.Input active={true} size={"small"} />}
-        </p>
-        订单信息：
-        <p>
-          <b>采购单号：</b>
-          {isName ? (
-            deviceData.order
-          ) : (
-            <Skeleton.Input active={true} size={"small"} />
-          )}
-        </p>
-        <p>
-          <b>下单时间：</b>
-          {formatDate(deviceData.order_time)}
-        </p>
-        <p>
-          <b>采购平台：</b>
-          {platfromType}
-        </p>
-        <p>
-          <b>支付方式：</b>
-          {payType}
-        </p>
+        <div className="flex  gap-2">
+          <Card
+            title="设备信息："
+            variant="borderless"
+             style={{ flex: 1 }}
+          >
+            <p>
+              <b>用途：</b>
+              {data.purpose}
+            </p>
+            <p>
+              <b>采购数量：</b>
+              {formatNumber(deviceData.numbers)}
+            </p>
+            <p>
+              <b>采购总价：</b>
+              {formatNumber(deviceData.total)} 元
+            </p>
+            <p>
+              <b>当前状态：</b>
+              {statusLabel(data.state)}
+            </p>
+            <p>
+              <b>分类：</b>
+              {data.category}
+            </p>
+          </Card>
+          <Card title="采购信息" variant="borderless"  style={{ flex: 1 }}>
+            <p>
+              <b>采购人员：</b>
+              {isName ? (
+                deviceData.purchaser
+              ) : (
+                <Skeleton.Input active={true} size={"small"} />
+              )}
+            </p>
+            <p>
+              <b>设备编号：</b>
+              {isName ? (
+                data.number
+              ) : (
+                <Skeleton.Input active={true} size={"small"} />
+              )}
+            </p>
+            <p>
+              <b>使用人员：</b>
+              {isName ? (
+                data.name
+              ) : (
+                <Skeleton.Input active={true} size={"small"} />
+              )}
+            </p>
+          </Card>
+        </div>
+        <Card title="订单信息" variant="borderless" className="mt-2">
+          <Space size={16}>
+            <p>
+              <b>采购单号：</b>
+              {isName ? (
+                deviceData.order
+              ) : (
+                <Skeleton.Input active={true} size={"small"} />
+              )}
+            </p>
+            <p>
+              <b>下单时间：</b>
+              {formatDate(deviceData.order_time)}
+            </p>
+          </Space>
+          <Space size={16}>
+            <p>
+              <b>采购平台：</b>
+              {platfromType}
+            </p>
+            <p>
+              <b>支付方式：</b>
+              {payType}
+            </p>
+          </Space>
+        </Card>
       </Card>
     </>
   );
