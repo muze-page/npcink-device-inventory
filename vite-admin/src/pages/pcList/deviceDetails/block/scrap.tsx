98cbc1948e056f763eb70fc1a44e250d99a8d10b
Muze-mac <13581261474@163.com>
2025-10-24 13:46:21 +0800

    优化写法

--- vite-admin/src/pages/pcList/deviceDetails/block/scrap.tsx
+++ vite-admin/src/pages/pcList/deviceDetails/block/scrap.tsx
@@ -12,46 +12,28 @@ interface Props {
 }
 const App: React.FC<Props> = ({ drawerData, residualValue }) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th className="w-28 text-center">
-            <Tooltip title="采购价对比二手价格的百分比">折旧率</Tooltip>
-          </th>
-          <th className="w-28 text-center">
-            <Tooltip title="按照设定百分比，相对于购买时间，当前剩下的价值">
-              残值
-            </Tooltip>
-          </th>
-          <th className="w-28 text-center">
-            <Tooltip title="采购价对比残值价格的百分比">残值率</Tooltip>
-          </th>
-        </tr>
-      </thead>
-
-      <tbody>
-        <tr>
-          <td className="w-28 text-center">
-            {/* 为啥可能是字符串 */}
-            {getPercentage(
-              Number(drawerData.depreciation),
-              Number(drawerData.purchase)
-            )}
-          </td>
-          <td className="w-28 text-center">{residualValue}元</td>
-          <td className="w-28 text-center">
-            {getPercentage(residualValue, drawerData.purchase)}
-          </td>
-        </tr>
-      </tbody>
-      <tfoot>
-        <tr>
-          <td colSpan={3} className="text-sm text-right py-2 text-zinc-400">
-            以上数据仅供参考
-          </td>
-        </tr>
-      </tfoot>
-    </table>
+    <>
+      <div className="flex">
+        <div className="flex-1">
+          <Tooltip title="采购价对比二手价格的百分比">折旧率：</Tooltip>
+          {/* 为啥可能是字符串 */}
+          {getPercentage(
+            Number(drawerData.depreciation),
+            Number(drawerData.purchase)
+          )}
+        </div>
+        <div className="flex-1">
+          <Tooltip title="按照设定百分比，相对于购买时间，当前剩下的价值">
+            残值：
+          </Tooltip>
+          {residualValue}元
+        </div>
+        <div className="flex-1">
+          <Tooltip title="采购价对比残值价格的百分比">残值率：</Tooltip>
+          {getPercentage(residualValue, drawerData.purchase)}
+        </div>
+      </div>
+    </>
   );
 };
 export default App;
